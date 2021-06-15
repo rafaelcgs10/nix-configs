bbca9cd592cf1c704f1fbf5cb7b02c8ce25c7474
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-06-14 22:58:47 -0300

    Remove chruby

--- doom.d/init.el
+++ doom.d/init.el
@@ -152,7 +152,7 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails +lsp +chruby)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +rails +lsp)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        scheme            ; a fully conniving family of lisps
