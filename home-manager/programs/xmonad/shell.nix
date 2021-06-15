9795321f446301c217f095b8f86f28ca9dd976fe
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-06-15 19:17:09 -0300

    Add missing haskell packages

--- home-manager/programs/xmonad/shell.nix
+++ home-manager/programs/xmonad/shell.nix
@@ -18,6 +18,8 @@ let
     nixpkgs.haskellPackages.haskell-language-server
     nixpkgs.haskellPackages.hlint
     nixpkgs.haskellPackages.hoogle
+    nixpkgs.haskellPackages.happy
+    nixpkgs.haskellPackages.haskell-src-exts
   ];
 in
 pkgs.stdenv.mkDerivation {
