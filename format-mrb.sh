05952188a3b7dabde51a8e9067f274f1112c6099
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-07-07 20:17:18 -0300

    Fix parted

--- format-mrb.sh
+++ format-mrb.sh
@@ -1,6 +1,6 @@
-sudo parted /dev/sda --mklabel msdos
-sudo parted /dev/sda --mkpart primary 1MiB -2GiB
-sudo parted /dev/sda --mkpart primary linux-swap -2GiB 100%
+sudo parted /dev/sda -- mklabel msdos
+sudo parted /dev/sda -- mkpart primary 1MiB -2GiB
+sudo parted /dev/sda -- mkpart primary linux-swap -2GiB 100%
 sudo mkfs.ext4 -L nixos /dev/sda1
 sudo mkswap -L swap /dev/sda2
 sudo mount /dev/disk/by-label/nixos /mnt
