4396aa7507015ad7fcfb16ee0d9d3c17a005de5b
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-07-09 16:32:27 -0300

    Add basic hardware config for raspberry pi 4

---
+++ nixos/raspberry-pi-4/hardware-configuration.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/5a6756294553fc3aa41e11563882db78c2dfbb4c.tar.gz" }/raspberry-pi/4"];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  # Enable GPU acceleration
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+}
