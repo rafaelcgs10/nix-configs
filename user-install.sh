257ae944db4f3450a38c38d294ca729a7063ed2c
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-07-10 21:18:52 -0300

    Remove lns from user install

--- user-install.sh
+++ user-install.sh
@@ -2,13 +2,9 @@ sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable/ nixpkgs-unst
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
 sudo nix-channel --update
 
-sudo rm /etc/nixos/configuration.nix
-sudo ln -s /home/rafael/nix-configs/nixos/configuration.nix /etc/nixos/
-cp /etc/nixos/hardware-configuration.nix /home/rafael/nix-configs/nixos/
-sudo rm /etc/nixos/hardware-configuration.nix
-sudo nixos-rebuild switch
-
 mkdir -p /home/rafael/.config/nixpkgs
 ln -s /home/rafael/nix-configs/home-manager/home.nix /home/rafael/.config/nixpkgs
 ln -s /home/rafael/nix-configs/home-manager/config.nix /home/rafael/.config/nixpkgs
+ln -s /home/rafael/nix-configs/home-manager/imports/raspberry-pi-4/default.nix /home/rafael/nix-configs/home-manager/imports/
+
 home-manager switch
