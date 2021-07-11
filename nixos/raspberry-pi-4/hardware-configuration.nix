65a8eb0562a6cd967f9bf37e6d68c587dd0ae04c
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-07-11 10:38:55 -0300

    Add samba server

--- nixos/raspberry-pi-4/hardware-configuration.nix
+++ nixos/raspberry-pi-4/hardware-configuration.nix
@@ -19,4 +19,36 @@
   hardware.raspberry-pi."4".fkms-3d.enable = true;
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+
+  services.xrdp.enable = true;
+  networking.firewall.allowedTCPPorts = [ 3389 ];
+  services.xrdp.defaultWindowManager = "xmonad";
+
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+    workgroup = WORKGROUP
+    server string = raspberry-server
+    netbios name = smbnix
+    security = user
+    #use sendfile = yes
+    #max protocol = smb2
+    hosts allow = 192.168.15  localhost
+    hosts deny = 0.0.0.0/0
+    guest account = nobody
+    map to guest = bad user
+  '';
+    shares = {
+      private = {
+        path = "/home/rafael/share";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "rafael";
+      };
+    };
+  };
 }
