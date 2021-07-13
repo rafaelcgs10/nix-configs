480ce8233bd4949b70d8dccd72b3a4bb3f82cb05
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-07-12 22:29:37 -0300

    Remove tree emacs icons dired use package

--- home-manager/programs/doom/doom.d/config.el
+++ home-manager/programs/doom/doom.d/config.el
@@ -207,11 +207,6 @@
   :after treemacs projectile
   :ensure t)
 
-(use-package! treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
 (use-package! treemacs-magit
   :after treemacs magit
   :ensure t)
