e5c68248aad00b80b0e19919e2d53df5f3d2d1ca
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-08-22 12:20:21 -0300

    Install printer

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,6 +79,7 @@ in {
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplipWithPlugin ];
 
   # Enable sound.
   sound.enable = true;
