7f3b554ca2cf569975f15665351df952cbf81212
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-08-28 21:25:01 -0300

    Add use cachix community

--- user-install.sh
+++ user-install.sh
@@ -1,6 +1,7 @@
 sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable/ nixpkgs-unstable
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
 sudo nix-channel --update
+cachix use nix-community
 
 mkdir -p ~/.config/nixpkgs
 ln -s ~/home-manager/home.nix ~/.config/nixpkgs
