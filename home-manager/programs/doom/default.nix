0999e6e12c8d8b32d2d700d9d2ffdf1cafcc3f91
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-09-12 18:22:00 -0300

    Emacs gcc

--- home-manager/programs/doom/default.nix
+++ home-manager/programs/doom/default.nix
@@ -1,27 +1,15 @@
 let
-  emacs-overlay = builtins.fetchTarball {url = https://github.com/nix-community/emacs-overlay/archive/539db709fdedeb4d9e470cd62e590c970ff7d627.tar.gz;};
+  emacs-overlay = builtins.fetchTarball {url = https://github.com/nix-community/emacs-overlay/archive/e00f171142307b3c9bb962beeaaf09d0254f9e31.tar.gz;};
   pkgs = import <nixpkgs> { overlays = [ (import emacs-overlay) ]; };
-  spinner-lzip = builtins.fetchurl {
-    url = "https://elpa.gnu.org/packages/spinner-1.7.3.el.lz";
-    sha256 = "188i2r7ixva78qd99ksyh3jagnijpvzzjvvx37n57x8nkp8jc4i4";
-  };
   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-    # url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
-    url = https://github.com/vlaci/nix-doom-emacs/archive/f2849e51a557b78214ab19bb504fb936d8d513d4.tar.gz;
+    url = https://github.com/vlaci/nix-doom-emacs/archive/33064319607745856f488a998ca3db8ffcede865.tar.gz;
+    # url = https://github.com/vlaci/nix-doom-emacs/archive/f2848e51a557b78214ab19bb504fb936d8d513d4.tar.gz;
     # url = https://github.com/vlaci/nix-doom-emacs/archive/3893c50877a9d2d5d4aeee524ba1539f22115f1f.tar.gz;
   }) {
+    # bundledPackages = false;
+    emacsPackages = pkgs.emacsPackagesFor pkgs.emacsGcc;
     # Directory containing your config.el init.el and packages.el files
     doomPrivateDir = ./doom.d;
-
-    # emacsPackagesOverlay = self: super: {
-    #   spinner = super.spinner.override {
-    #     elpaBuild = args: super.elpaBuild (args // {
-    #       src = pkgs.runCommandLocal "spinner-1.7.3.el" {} ''
-    #         ${pkgs.lzip}/bin/lzip -d -c ${spinner-lzip} > $out
-    #       '';
-    #     });
-    #   };
-    # };
   };
 in {
   home.packages = [ doom-emacs pkgs.python3 ];
