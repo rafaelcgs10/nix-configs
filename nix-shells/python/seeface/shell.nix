f6083b641ff306167618ea081e90d652220f6e0c
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-10-28 21:57:21 -0300

    Add instructions

--- nix-shells/python/seeface/shell.nix
+++ nix-shells/python/seeface/shell.nix
@@ -4,13 +4,14 @@ mkShell {
   buildInputs = [
     python38Packages.virtualenv # run virtualenv .
     python38Packages.numpy
-    python38Packages.opencv3 
+    python38Packages.opencv3
     python38Packages.pillow
     python38Packages.pillow
     python38Packages.onnx
-    python38Packages.pip 
+    python38Packages.pip
   ];
   shellHook = ''
+    # https://pastebin.com/t86VmYCc
     # fixes libstdc++ issues and libgl.so issues
     LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
     virtualenv -p python3 env
