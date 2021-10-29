c5b56a9918c063a5f774fb24932c46fe6e8584a3
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-10-28 21:55:22 -0300

    Python seeface

---
+++ nix-shells/python/seeface/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import <nixpkgs> {}).pkgs }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    python38Packages.virtualenv # run virtualenv .
+    python38Packages.numpy
+    python38Packages.opencv3 
+    python38Packages.pillow
+    python38Packages.pillow
+    python38Packages.onnx
+    python38Packages.pip 
+  ];
+  shellHook = ''
+    # fixes libstdc++ issues and libgl.so issues
+    LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
+    virtualenv -p python3 env
+    source env/bin/activate
+  '';
+}
