662c896e18ec6c5ddedffcb4c31eb3502712e658
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-12-24 15:56:58 -0300

    Remove qutebrowser

--- home-manager/programs/non-arm/default.nix
+++ home-manager/programs/non-arm/default.nix
@@ -2,8 +2,6 @@
 
 let
   unstable = import <nixpkgs-unstable> { config.allowUnfree = true; overlays = [(self: super: { discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball "https://discord.com/api/download?platform=linux&format=tar.gz"; });})];};
-  solarized-everything-css = pkgs.callPackage ../solarized-everything-css { };
-  css = "${solarized-everything-css}/share/css";
 in
 {
   home.packages = [
@@ -20,19 +18,4 @@ in
     pkgs.google-cloud-sdk
     pkgs.vivaldi
   ];
-
-  programs.qutebrowser = {
-    enable = true;
-    settings = {
-      auto_save.session = true;
-      spellcheck.languages = [ "en-US" "pt-BR" ];
-      colors.webpage.preferred_color_scheme = "dark";
-    };
-
-    keyBindings = {
-      normal = {
-        "<Ctrl-r>" = "config-cycle content.user_stylesheets '${css}/darculized-all-sites.css' ''";
-      };
-    };
-  };
 }
