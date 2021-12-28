da64f764494c943ea48d39045126ddefacf91e62
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-12-28 13:04:43 -0300

    Upgrade versions

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,7 +4,7 @@ let
 in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  home.stateVersion = "21.03";
+  home.stateVersion = "21.05";
 
   imports = [
     ./imports/default.nix
