2d99caa396ee814a12f84731f95f58660bef1955
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-12-30 12:29:11 -0300

    Add thinkpad config

---
+++ nixos/thinkpad-e14/boot-loader.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+}
