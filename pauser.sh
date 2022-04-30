9ba67bcc75355f0bb057087c97e63ec309737ea2
rafaelcgs10 <rafaelcgs10@gmail.com>
2022-04-30 04:35:43 -0300

    Fix to make it work with wine

--- pauser.sh
+++ pauser.sh
@@ -2,4 +2,4 @@ focused_window_id=$(xdotool getwindowfocus)
 active_window_id=$(xdotool getactivewindow)
 active_window_pid=$(xdotool getwindowpid "$active_window_id")
 
-kill -STOP ${active_window_pid}
+kill -STOP ${active_window_pid} || ps aux | grep .exe | grep Program | awk '{print $2}' | xargs -I XX sh -c 'kill -STOP XX' 
--- unpauser.sh
+++ unpauser.sh
@@ -2,4 +2,4 @@ focused_window_id=$(xdotool getwindowfocus)
 active_window_id=$(xdotool getactivewindow)
 active_window_pid=$(xdotool getwindowpid "$active_window_id")
 
-kill -CONT ${active_window_pid}
+kill -CONT ${active_window_pid} || ps aux | grep .exe | grep Program | awk '{print $2}' | xargs -I XX sh -c 'kill -CONT XX'
