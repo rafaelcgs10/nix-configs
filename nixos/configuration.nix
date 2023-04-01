3155c47b061a5c0abe54bee7354c220652704612
rafaelcgs10 <rafaelcgs10@gmail.com>
2023-04-01 15:38:03 +0200

    Timezone

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,7 +38,7 @@ in {
   #   arguments = [ "-config" "7de4a9" "-cache-size" "20MB" ];
   # };
   # Set your time zone.
-  time.timeZone = null;
+  time.timeZone = "Europe/Copenhagen";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
