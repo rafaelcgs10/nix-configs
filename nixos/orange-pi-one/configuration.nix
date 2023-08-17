00e1944c686ff9c2d9689ed0804c48d45c549a50
rafaelcgs10 <rafaelcgs10@gmail.com>
2023-08-17 12:57:50 +0200

    Fix orange pi wifi

--- nixos/orange-pi-one/configuration.nix
+++ nixos/orange-pi-one/configuration.nix
@@ -7,12 +7,23 @@
 
   nix.binaryCaches = lib.mkForce [ "https://cache.armv7l.xyz" ];
   nix.binaryCachePublicKeys = [ "cache.armv7l.xyz-1:kBY/eGnBAYiqYfg0fy0inWhshUo+pGFM3Pj7kIkmlBk=" ];
-    
 
-  networking.hostName = "orange-pi-one";
+ #  networking.networkmanager = {
+ #    enable = true;
+ #   # dns = "none";
+ #   wifi.powersave = false;
+ #   extraConfig = ''
+ #      [main]
+ #      rc-manager=file
+ #   '';
+ # };
+
+ networking.hostName = "orange-pi-one";
   #networking.hostId = "24ebc6f2";
-  networking.wireless.enable = false;
-  networking.wireless.iwd.enable = true;
+  networking.wireless.enable = true;
+  # networking.wireless.iwd.enable = true;
+  networking.wireless.userControlled.enable = true;
+
 
   security.sudo.enable = true;
   services.openssh.enable = true;
@@ -27,7 +38,7 @@
     htop
     git
     wirelesstools
-    wireguard-tools
+    wpa_supplicant
     # sysbench
     syncthing
     pkg-config
@@ -36,9 +47,15 @@
     # libdbusmenu
   ];
 
-  system.stateVersion = "23.05"; # Did you read the comment?
+
+  networking.wireless.networks.Itsawonderfulwifi.pskRaw = "9c14b5c4dd05c191fd73fc96f53d28462058e826867813d3b41b1b14a14076fc";
+
+
+  system.stateVersion = "23.05";
 
   boot.kernelPackages = lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.extraModulePackages = with config.boot.kernelPackages; [rtw88 rtl8821cu];
+
 
   users.extraUsers.rafael = {
     isNormalUser = true;
