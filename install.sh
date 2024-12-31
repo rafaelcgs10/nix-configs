1829141c965d0c7fcb1091186fbe13e3ad0bd1b7
rafaelcgs10 <rafaelcgs10@gmail.com>
2024-12-31 10:48:24 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -4,9 +4,16 @@ sudo nix-channel --add https://github.com/nix-community/home-manager/archive/10e
 sudo nix-channel --update
 cachix use nix-community
 
-mkdir -p ~/.config/nixpkgs
-ln -s ~/nix-configs/home-manager/home.nix ~/.config/nixpkgs
-ln -s ~/nix-configs/home-manager/config.nix ~/.config/nixpkgs
+sudo mv /etc/nixos/configuration.nix ./nixos/configuration.nix_bk
+sudo mv /etc/nixos/hardware-configuration.nix ./nixos/hardware-configuration.nix_bk
+
+sudo ln -s ~/nix-configs/nixos/configuration.nix /etc/nixos
+
+sudo nixos-rebuild switch
+
+mkdir -p ~/.config/home-manager
+ln -s ~/nix-configs/home-manager/home.nix ~/.config/home-manager
+ln -s ~/nix-configs/home-manager/config.nix ~/.config/home-manager
 ln -s ~/nix-configs/home-manager/imports/default/default.nix ~/nix-configs/home-manager/imports/
 
 home-manager switch
