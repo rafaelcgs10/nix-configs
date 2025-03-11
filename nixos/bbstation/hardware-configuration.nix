dc4718d814d56785c336a10b3be7d3c6ec31cbba
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-03-11 16:36:45 +0100

    Upgrade nvidia

--- nixos/bbstation/hardware-configuration.nix
+++ nixos/bbstation/hardware-configuration.nix
@@ -3,19 +3,23 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
+let
+  unstable = import <nixpkgs-unstable> { config.allowUnfree = true; };
+in
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   # boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = unstable.linuxPackages;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.supportedFilesystems = [ "nfs" ];
 
-  boot.blacklistedKernelModules = [ "rtl8821cu" "rtw88_8821cu" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl88xxau-aircrack ];
+  # boot.blacklistedKernelModules = [ "rtl8821cu" "rtw88_8821cu" ];
+  # boot.extraModulePackages = with config.boot.kernelPackages; [ rtl88xxau-aircrack ];
   # boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8821au ];
   # boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8821cu ];
   hardware.wirelessRegulatoryDatabase = true;
@@ -88,11 +92,12 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = unstable.linuxPackages.nvidiaPackages.stable;
   };
   powerManagement = {
     cpuFreqGovernor = "performance";
   };
+
 
   # Docker config
   virtualisation.docker = {
