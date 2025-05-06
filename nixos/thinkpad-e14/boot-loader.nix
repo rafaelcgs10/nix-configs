c7a9c2c3fd336779f918221110f846da7ac8462e
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-05-06 09:30:48 +0200

    Boot loader windows

--- nixos/thinkpad-e14/boot-loader.nix
+++ nixos/thinkpad-e14/boot-loader.nix
@@ -2,7 +2,30 @@
 
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+
+    systemd-boot = {
+      enable = true;
+
+      windows = {
+        "windows" =
+          let
+            # To determine the name of the windows boot drive, boot into edk2 first, then run
+            # `map -c` to get drive aliases, and try out running `FS1:`, then `ls EFI` to check
+            # which alias corresponds to which EFI partition.
+            boot-drive = "HD0b";
+          in
+          {
+            title = "Windows";
+            efiDeviceHandle = boot-drive;
+            sortKey = "y_windows";
+          };
+      };
+
+      edk2-uefi-shell.enable = true;
+      edk2-uefi-shell.sortKey = "z_edk2";
+    };
+  };
   boot.loader.grub.useOSProber = true;
 }
