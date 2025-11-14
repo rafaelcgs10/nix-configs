400b007f6165a23d488f1bee0b3ce52a009bf6c6
rafaelcgs10 <rafaelcgs10@gmail.com>
2025-11-14 20:04:17 +0100

    Add flake file

---
+++ nixos/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    home-manager.url = github:nix-community/home-manager;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =  inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }: {
+    nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
