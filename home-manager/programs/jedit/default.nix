8497c089f9ed425e0b892aae30b66bebf1d4a7dc
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-01-21 16:51:38 +0100

    Update Isabelle

--- home-manager/programs/jedit/default.nix
+++ home-manager/programs/jedit/default.nix
@@ -2,15 +2,15 @@
 
 let
   newer_isabelle_pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/8b5d0ebbbd6375f1ecd0ce169d44d03388189038.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/6a1b486ad3b39263e651cd772b4051cccf218634.tar.gz";
   }) {};
 in
 {
-  home.packages = with pkgs; [
+  home.packages = with newer_isabelle_pkgs; [
     (isabelle)
     # (isabelle.withComponents (p: [ p.isabelle-linter ]))
   ];
-  home.file.".isabelle/Isabelle2025/jedit/properties".text = builtins.readFile ./properties;
+  # home.file.".isabelle/Isabelle2025-2/jedit/properties".text = builtins.readFile ./properties;
 # home.file.".isabelle/isabelle-linter" = {
 #   source =  builtins.fetchTarball {
 #     url = "https://github.com/isabelle-prover/isabelle-linter/archive/refs/tags/Isabelle2022-v1.2.1.tar.gz";
@@ -23,7 +23,7 @@ in
 #     sha256 = "0k5a9w2gqzlp5cxm71dj4xddbni7p063x7pdxwyn86jdrbwqm1b4";
 #   };
 # };
-home.file.".isabelle/Isabelle2025/etc/components".text = ''
+home.file.".isabelle/Isabelle2025-2/etc/components".text = ''
       /home/rafael/.isabelle/afp/thys
   '';
 }
