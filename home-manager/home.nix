85cf75b78845270f2494e16cb2fdb6a5b6f7db1e
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-04-01 21:28:41 +0200

    Add agx-emulsion

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -181,7 +181,7 @@ in {
     # (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Mononoki" ]; })
 
     # pkgs.rawtherapee
-
+    (import ./programs/agx-emulsion/default.nix {})
   ];
 
   home.enableNixpkgsReleaseCheck = false;
