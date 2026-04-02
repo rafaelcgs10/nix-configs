139713572c6dd496e1f3fce1440cdac5e254c699
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-04-02 10:12:41 +0200

    More on Isabelle docker

--- docker/isabelle/entrypoint.sh
+++ docker/isabelle/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xpra start --bind-tcp=0.0.0.0:14500 --html=on --daemon=no --start="/home/ubuntu/Isabelle2025-2/bin/isabelle jedit -d . -R Dataplane"
+xpra start --bind-tcp=0.0.0.0:14500 --html=on --daemon=no --start="/home/ubuntu/Isabelle2025-2/bin/isabelle jedit -d ."
