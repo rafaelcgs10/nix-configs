020650f28fc6feec138a8fcce2061fd1289fb6a8
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-07-11 16:03:15 +0200

    Mount sd

--- nixos/bbtablet/hardware-configuration.nix
+++ nixos/bbtablet/hardware-configuration.nix
@@ -91,6 +91,36 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
+  fileSystems."/home/rafael/sd" = {
+    device = "/dev/disk/by-label/sd";
+    fsType = "btrfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "nofail"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "compress=zstd"
+      "noatime"
+    ];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /home/rafael/sd 0755 rafael users -"
+  ];
+
+  systemd.services.sd-storage-permissions = {
+    description = "Ensure Rafael owns the SD card mount root";
+    wantedBy = [ "home-rafael-sd.mount" ];
+    after = [ "home-rafael-sd.mount" ];
+    bindsTo = [ "home-rafael-sd.mount" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      chown rafael:users /home/rafael/sd
+    '';
+  };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/5e97a962-a45b-43ac-b3a8-16da7888aa65"; }
     ];
