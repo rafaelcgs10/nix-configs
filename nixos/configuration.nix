89195783ddba59170742bd053921e151941c10f8
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-08-09 11:14:30 +0200

    Reduce timeout

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -568,6 +568,13 @@ in {
 
   # ];
 
+  # set default timeout to 10s - many times reboot waits 90s
+  systemd.settings = {
+    Manager = {
+      DefaultTimeoutStopSec = "10s";
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
